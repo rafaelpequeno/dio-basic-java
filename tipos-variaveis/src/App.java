26ce8410b7bcd6623d91411373b6cefafa7ec84c
Rafael <rflpeq.rs@gmail.com>
2023-10-10 14:21:10 -0300

    Adiciona arquivos de testes e desafio contador

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        final long CEP = 13451176L;
+        System.out.println(CEP);
+        int numero = 5;
+        System.out.println(++numero);
+        // char initial = 'R';
     }
 }
